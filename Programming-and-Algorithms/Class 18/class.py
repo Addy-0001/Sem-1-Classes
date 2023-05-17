c60ed7760fc4f05e420f8788b0fb831022a57ded
Adamya Neupane <adamsneupane@gmail.com>
2023-05-17 12:25:15 +0545

    get rekt lol

---
+++ Programming-and-Algorithms/Class 18/class.py
@@ -0,0 +1,9 @@
+add = lambda a, b : a + b
+sub = lambda a, b : a - b
+mul = lambda a, b : a * b
+div = lambda a, b : a / b
+
+print(add(10, 20))
+print(sub(10, 20))
+print(mul(10, 20))
+print(div(10, 20))
